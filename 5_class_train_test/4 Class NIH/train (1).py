5543a3614bda913ca25c690a530c8a999f75d278
GAUTAM KUMAR <65457437+gautamHCSCV@users.noreply.github.com>
2022-05-31 11:38:19 +0530

    Add files via upload

---
+++ 5_class_train_test/4 Class NIH/train (1).py
@@ -0,0 +1,214 @@
+import os
+import json
+import time
+import random
+import numpy as np
+import pandas as pd
+from PIL import Image
+from sklearn.model_selection import train_test_split
+import torch.nn.functional as F
+import pickle as pkl
+import warnings
+warnings.filterwarnings("ignore")
+
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import torchvision.models as models
+
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"  
+os.environ["CUDA_VISIBLE_DEVICES"]="7"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+
+config = dict(
+    saved_path="saved4/resnet.pt",
+    best_saved_path = "saved4/resnet_best.pt",
+    lr=0.001, 
+    EPOCHS = 30,
+    BATCH_SIZE = 32,
+    IMAGE_SIZE = 224,
+    TRAIN_VALID_SPLIT = 0.2,
+    device=device,
+    SEED = 42,
+    pin_memory=True,
+    num_workers=3,
+    USE_AMP = True,
+    channels_last=False)
+
+random.seed(config['SEED'])
+# If you or any of the libraries you are using rely on NumPy, you can seed the global NumPy RNG 
+np.random.seed(config['SEED'])
+# Prevent RNG for CPU and GPU using torch
+torch.manual_seed(config['SEED'])
+torch.cuda.manual_seed(config['SEED'])
+
+torch.backends.cudnn.benchmarks = True
+torch.backends.cudnn.deterministic = True
+
+#torch.backends.cuda.matmul.allow_tf32 = True
+
+# The flag below controls whether to allow TF32 on cuDNN. This flag defaults to True.
+#torch.backends.cudnn.allow_tf32 = True
+
+data_transforms = {
+    'test': transforms.Compose([
+        transforms.Resize((config['IMAGE_SIZE'],config['IMAGE_SIZE'])),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+my_path = '../../../dataset/NIH_WHOLE_DATASET/nih-6-class'
+
+images = torchvision.datasets.ImageFolder(root=my_path,transform=data_transforms['test'])
+print(len(images))
+print(images.class_to_idx)
+
+l = len(images)+1
+while l>len(images):
+    l = len(images)
+    for path in images.imgs:
+        if ord(path[0][path[0].index('6')+8])>ord('F'):
+            images.imgs.remove(path)
+print(len(images))
+train_data,valid_data,test_data = torch.utils.data.dataset.random_split(images,[30000,2000,319])
+
+train_dl = torch.utils.data.DataLoader(dataset=train_data,batch_size=config['BATCH_SIZE'],shuffle=True, num_workers = config['num_workers'],
+                                          pin_memory = config['pin_memory'])
+valid_dl = torch.utils.data.DataLoader(dataset = valid_data,batch_size=config['BATCH_SIZE'],shuffle=True, num_workers = 
+                                          config['num_workers'], pin_memory = config['pin_memory'])
+test_dl = torch.utils.data.DataLoader(dataset=test_data,batch_size=config['BATCH_SIZE'],shuffle=True, num_workers = config['num_workers'],
+                                          pin_memory = config['pin_memory'])
+
+
+resnet50 = torchvision.models.resnet50(pretrained=True)
+resnet50.fc = nn.Sequential(
+        nn.Dropout(0.4),
+        nn.Linear(in_features = 2048, out_features = 4, bias = True))
+model = resnet50
+
+print(model)
+
+
+model = model.to(config['device'])
+optimizer = optim.Adam(model.parameters(),lr=config['lr'])
+criterion = nn.CrossEntropyLoss()
+
+
+def train_model(model,criterion,optimizer,num_epochs=10):
+
+    since = time.time()                                            
+    batch_ct = 0
+    example_ct = 0
+    best_acc = 0.3
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 25)
+        run_corrects = 0
+        #Training
+        model.train()
+        for x,y in train_dl: #BS=32 ([BS,3,224,224], [BS,4])            
+            if config['channels_last']:
+                x = x.to(config['device'], memory_format=torch.channels_last) #CHW --> #HWC
+            else:
+                x = x.to(config['device'])
+            y = y.to(config['device']) #CHW --> #HWC
+            
+            
+            
+            optimizer.zero_grad()
+            #optimizer.zero_grad(set_to_none=True)
+            ######################################################################
+            
+            train_logits = model(x) #Input = [BS,3,224,224] (Image) -- Model --> [BS,4] (Output Scores)
+            
+            _, train_preds = torch.max(train_logits, 1)
+            train_loss = criterion(train_logits,y)
+            train_loss = criterion(train_logits,y)
+            run_corrects += torch.sum(train_preds == y.data)
+            
+            train_loss.backward() # Backpropagation this is where your W_gradient
+            loss=train_loss
+
+            optimizer.step() # W_new = W_old - LR * W_gradient 
+            example_ct += len(x) 
+            batch_ct += 1
+            if ((batch_ct + 1) % 700) == 0:
+                train_log(loss, example_ct, epoch)
+            ########################################################################
+        
+        #validation
+        model.eval()
+        running_loss = 0.0
+        running_corrects = 0
+        total = 0
+        # Disable gradient calculation for validation or inference using torch.no_rad()
+        with torch.no_grad():
+            for x,y in valid_dl:
+                if config['channels_last']:
+                    x = x.to(config['device'], memory_format=torch.channels_last) #CHW --> #HWC
+                else:
+                    x = x.to(config['device'])
+                y = y.to(config['device'])
+                valid_logits = model(x)
+                _, valid_preds = torch.max(valid_logits, 1)
+                valid_loss = criterion(valid_logits,y)
+                running_loss += valid_loss.item() * x.size(0)
+                running_corrects += torch.sum(valid_preds == y.data)
+                total += y.size(0)
+            
+        epoch_loss = running_loss / len(valid_data)
+        epoch_acc = running_corrects.double() / len(valid_data)
+        train_acc = run_corrects.double() / len(train_data)
+        print("Train Accuracy",train_acc.cpu())
+        print("Validation Loss is {}".format(epoch_loss))
+        print("Validation Accuracy is {}\n".format(epoch_acc.cpu()))
+        if epoch_acc.cpu()>best_acc:
+            print('One of the best validation accuracy found.\n')
+            torch.save(model.state_dict(), config['best_saved_path'])
+            best_acc = epoch_acc.cpu()
+
+            
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    
+    torch.save(model.state_dict(), config['saved_path'])
+
+    
+def train_log(loss, example_ct, epoch):
+    loss = float(loss)
+    print(f"Loss after " + str(example_ct).zfill(5) + f" examples: {loss:.3f}")
+    
+train_model(model, criterion, optimizer, num_epochs=config['EPOCHS'])
+
+model.eval()
+running_loss = 0.0
+running_corrects = 0
+total = 0
+# Disable gradient calculation for validation or inference using torch.no_rad()
+with torch.no_grad():
+            for x,y in test_dl:
+                if config['channels_last']:
+                    x = x.to(config['device'], memory_format=torch.channels_last) #CHW --> #HWC
+                else:
+                    x = x.to(config['device'])
+                y = y.to(config['device']) #CHW --> #HWC
+                valid_logits = model(x)
+                _, valid_preds = torch.max(valid_logits, 1)
+                valid_loss = criterion(valid_logits,y)
+                running_loss += valid_loss.item() * x.size(0)
+                running_corrects += torch.sum(valid_preds == y.data)
+                total += y.size(0)
+            
+epoch_loss = running_loss / len(test_data)
+epoch_acc = running_corrects.double() / len(test_data)
+print("Test Loss is {}".format(epoch_loss))
+print("Test Accuracy is {}".format(epoch_acc.cpu()))
+
